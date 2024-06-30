f850e151b59076488448c0f2ec14a0b205ab9ab0
zane <39070793+zaneenders@users.noreply.github.com>
2024-06-30 13:03:01 -0600

    github instead of local

--- Package.swift
+++ Package.swift
@@ -10,26 +10,22 @@ let package = Package(
         .library(name: "ChromaShell", targets: ["Chroma", "ChromaShell"])
     ],
     dependencies: [
-        // .package(
-        //     url: "https://github.com/apple/swift-nio.git",
-        //     from: "2.66.0"),
-
-        .package(name: "swift-nio", path: "../../.repositories/swift-nio"),
+        .package(
+            url: "https://github.com/apple/swift-nio.git",
+            from: "2.66.0"),
         /*
         Below are Package dependencies but not for output. Comment out if not
         needed for faster build times.
         */
-        // .package(
-        //     url: "https://github.com/apple/swift-format.git",
-        //     from: "510.1.0"),
         .package(
-            name: "swift-format", path: "../../.repositories/swift-format"),
+            url: "https://github.com/apple/swift-format.git",
+            from: "510.1.0"),
         // View documentation locally with the following command
         // swift package --disable-sandbox preview-documentation --target ChromaShell
         // swift package --disable-sandbox preview-documentation --target Chroma
-        // .package(
-        //     url: "https://github.com/apple/swift-docc-plugin.git",
-        //     from: "1.3.0"),
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin.git",
+            from: "1.3.0"),
     ],
     targets: [
         .executableTarget(
